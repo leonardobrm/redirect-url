0490af22352ece4fc1ec720cc25324ab0339f14f
Leonardo Brasil Moreira <leonardo.brasil59@outlook.com>
2025-05-03 16:05:39 -0300

    :rocket: init project

---
+++ src/main/java/com/bagsafe/redirecturlapi/RedirectUrlApiApplication.java
@@ -0,0 +1,13 @@
+package com.bagsafe.redirecturlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectUrlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedirectUrlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bagsafe/redirecturlapi/RedirectUrlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagsafe.redirecturlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectUrlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
